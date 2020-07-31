47bf9f4cd9c8a0563eadd935b66c0c2bfcc0e348
Jeremy Fleshman <jeremy.fleshman@icloud.com>
2020-07-31 16:41:48 -0500

    - Formatted HomeTableViewController.swift according to current swift-format rules

--- HWS_100DoS_Day32ChallengeApp/HomeTableViewController.swift
+++ HWS_100DoS_Day32ChallengeApp/HomeTableViewController.swift
@@ -9,77 +9,114 @@
 import UIKit
 
 class HomeTableViewController: UITableViewController {
-    // MARK: - Properties
-    var shoppingItems = [String]()
+  // MARK: - Properties
 
-    // MARK: - Lifecycle methods
-    override func viewDidLoad() {
-        super.viewDidLoad()
+  @IBOutlet var uiview: UIView!
 
-        configureNavigationBar()
+  var shoppingItems = [String]()
+
+  // MARK: - Lifecycle methods
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+
+    configureNavigationBar()
+  }
+
+  fileprivate func configureNavigationBar() {
+    title = "Shopping list"
+
+    navigationItem.rightBarButtonItem = UIBarButtonItem(
+      barButtonSystemItem: .add,
+      target: self,
+      action: #selector(addItem)
+    )
+
+    navigationItem.leftBarButtonItem = UIBarButtonItem(
+      barButtonSystemItem: .trash,
+      target: self,
+      action: #selector(resetList)
+    )
+  }
+
+  // MARK: Selector methods
+
+  @objc func addItem() {
+    let alert = UIAlertController(
+      title: "Add item to list?",
+      message: nil,
+      preferredStyle: .alert
+    )
+
+    let addAction = UIAlertAction(title: "Add", style: .default) {
+      [weak self] _ in guard let strongSelf = self else { return }
+
+      if let item = alert.textFields?[0].text, !item.isEmpty {
+        strongSelf.shoppingItems.insert(item, at: 0)
+        strongSelf.tableView.insertRows(
+          at: [IndexPath(row: 0, section: 0)],
+          with: .automatic
+        )
+      }
     }
 
-    fileprivate func configureNavigationBar() {
-        title = "Shopping list"
+    let cancelAction = UIAlertAction(
+      title: "Cancel",
+      style: .cancel,
+      handler: nil
+    )
 
-        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(addItem))
+    alert.addTextField(configurationHandler: nil)
+    alert.addAction(addAction)
+    alert.addAction(cancelAction)
+    present(alert, animated: true)
+  }
 
-        navigationItem.leftBarButtonItem = UIBarButtonItem(barButtonSystemItem: .trash, target: self, action: #selector(resetList))
+  @objc func resetList() {
+    let alert = UIAlertController(
+      title: "Reset the list?",
+      message: "This will delete all current shopping items",
+      preferredStyle: .alert
+    )
+
+    let deleteAction = UIAlertAction(title: "Delete", style: .destructive) {
+      [weak self] _ in guard let strongSelf = self else { return }
+
+      strongSelf.shoppingItems = []
+      strongSelf.tableView.reloadData()
     }
 
-    // MARK: Selector methods
-    @objc func addItem() {
-        let alert = UIAlertController(title: "Add item to list?", message: nil, preferredStyle: .alert)
+    let cancelAction = UIAlertAction(
+      title: "Cancel",
+      style: .cancel,
+      handler: nil
+    )
 
-        let addAction = UIAlertAction(title: "Add", style: .default) { [weak self] _ in
-            guard let strongSelf = self else { return }
+    alert.addAction(cancelAction)
+    alert.addAction(deleteAction)
 
-            if let item = alert.textFields?[0].text, !item.isEmpty {
-                strongSelf.shoppingItems.insert(item, at: 0)
-                strongSelf.tableView.insertRows(at: [IndexPath(row: 0, section: 0)], with: .automatic)
-            }
-        }
-
-        let cancelAction = UIAlertAction(title: "Cancel", style: .cancel, handler: nil)
-
-        alert.addTextField(configurationHandler: nil)
-        alert.addAction(addAction)
-        alert.addAction(cancelAction)
-        present(alert, animated: true)
-    }
-
-    @objc func resetList() {
-        let alert = UIAlertController(title: "Reset the list?", message: "This will delete all current shopping items", preferredStyle: .alert)
-
-        let deleteAction = UIAlertAction(
-            title: "Delete",
-            style: .destructive) { [weak self] _ in
-                guard let strongSelf = self else { return }
-
-                strongSelf.shoppingItems = []
-                strongSelf.tableView.reloadData()
-        }
-
-        let cancelAction = UIAlertAction(title: "Cancel", style: .cancel, handler: nil)
-
-        alert.addAction(cancelAction)
-        alert.addAction(deleteAction)
-
-        present(alert, animated: true)
-    }
+    present(alert, animated: true)
+  }
 }
 
 // MARK: - UITableView methods
+
 extension HomeTableViewController {
-    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return shoppingItems.count
-    }
+  override func tableView(_: UITableView, numberOfRowsInSection _: Int) -> Int {
+    shoppingItems.count
+  }
 
-    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "Item", for: indexPath)
+  override func tableView(
+    _ tableView: UITableView,
+    cellForRowAt indexPath: IndexPath
+  ) -> UITableViewCell {
+    let cell = tableView.dequeueReusableCell(
+      withIdentifier: "Item",
+      for: indexPath
+    )
 
-        cell.textLabel?.text = shoppingItems[indexPath.row]
+    cell.textLabel?.text = shoppingItems[indexPath.row]
 
-        return cell
-    }
+    return cell
+  }
 }
